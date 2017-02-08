7ac2b0935029f6673b8c7eb80c340984bf7515a8
ao-streetart <aostreetart9@gmail.com>
2017-02-07 20:33:38 -0500

    Ensure docker images are linked in Travis CI Container

--- scripts/linux/run_docker_instance.sh
+++ scripts/linux/run_docker_instance.sh
@@ -4,9 +4,11 @@ BRANCH_NAME=${1:-master}
 CONSUL_ADDR=${2:-"localhost:8500"}
 IP=${3:-localhost}
 PORT=${4:-5555}
+LINK_NAME=${5:-database}
+LINK_IMAGE=${6:-neo4j}
 
 if [ $BRANCH_NAME == "master" ]; then
-  docker run --name crazyivan -d aostreetart/crazyivan -consul-addr=$CONSUL_ADDR -ip=$IP -port=$PORT &
+  docker run --name crazyivan --link $LINK_NAME:$LINK_IMAGE -d aostreetart/crazyivan -consul-addr=$CONSUL_ADDR -ip=$IP -port=$PORT &
 else
-  docker run --name crazyivan -d aostreetart/crazyivan:$BRANCH_NAME -consul-addr=$CONSUL_ADDR -ip=$IP -port=$PORT &
+  docker run --name crazyivan --link $LINK_NAME:$LINK_IMAGE -d aostreetart/crazyivan:$BRANCH_NAME -consul-addr=$CONSUL_ADDR -ip=$IP -port=$PORT &
 fi
