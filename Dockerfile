af51d6da5599edb73e1db3e48190ce48e4cf04f0
ao-streetart <aostreetart9@gmail.com>
2017-03-04 00:46:46 -0500

    Docker Fix

--- Dockerfile
+++ Dockerfile
@@ -106,5 +106,7 @@ EXPOSE 12345
 #Expose the 5000 port range for DVS Services
 EXPOSE 5000-5999
 
+RUN git clone https://github.com/AO-StreetArt/CrazyIvan.git
+
 #Build & Start up Crazy Ivan with script as entry point
 ENTRYPOINT ["CrazyIvan/scripts/linux/start_docker_instance.sh"]
--- scripts/linux/start_docker_instance.sh
+++ scripts/linux/start_docker_instance.sh
@@ -5,23 +5,17 @@ IP_ARG=$2
 PORT_ARG=$3
 LOG_ARG=$4
 BRANCH_NAME=${5:-latest}
-BASE_DIR=${6:-/usr/local/etc}
-
-mkdir $BASE_DIR/CrazyIvan
-
-#Pull the project source from github
-git clone https://github.com/AO-StreetArt/CrazyIvan.git $BASE_DIR/CrazyIvan
 
 if [ $BRANCH_NAME != "latest" ]; then
-  cd $BASE_DIR/CrazyIvan && git checkout $BRANCH_NAME && git pull origin $BRANCH_NAME
+  cd CrazyIvan && git checkout $BRANCH_NAME && git pull origin $BRANCH_NAME
 fi
 
 # Build the Project & Unit Tests
-cd $BASE_DIR/CrazyIvan && make && make test
+cd CrazyIvan && make && make test
 
 # Execute Unit Tests
-cd $BASE_DIR/CrazyIvan && ./scene_test
-cd $BASE_DIR/CrazyIvan && ./configuration_test -config-file=src/test/test.properties
+cd CrazyIvan && ./scene_test
+cd CrazyIvan && ./configuration_test -config-file=src/test/test.properties
 
 #Start up Crazy Ivan
-cd $BASE_DIR/CrazyIvan && ./crazy_ivan $CONSUL_ARG $IP_ARG $PORT_ARG $LOG_ARG
+cd CrazyIvan && ./crazy_ivan $CONSUL_ARG $IP_ARG $PORT_ARG $LOG_ARG
