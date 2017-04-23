7c779f94010e25b08547c98a407b33a0eb615139
Alex Barry <aostreetart9@gmail.com>
2017-04-23 02:06:29 -0400

    Testing Fixes

--- scripts/linux/build_docker_instance.sh
+++ scripts/linux/build_docker_instance.sh
@@ -6,7 +6,7 @@ DIR=$2
 TAG_NAME="latest"
 
 if [ $BRANCH_NAME != "master" ]; then
-  cd $DIR && sudo docker build --file DebugDockerfile -t "aostreetart/crazyivan:$BRANCH_NAME" .
+  cd $DIR && sudo docker build --file DevelDockerfile -t "aostreetart/crazyivan:$BRANCH_NAME" .
   TAG_NAME=$BRANCH_NAME
 else
   cd $DIR && sudo docker build -t "aostreetart/crazyivan:$TAG_NAME" .
