31e31169448aaa43f60d2e49cf42725ca5157446
Alex Barry <aostreetart9@gmail.com>
2018-06-05 02:36:09 -0400

    change yum to apt-get in container install script

--- scripts/linux/container_install.sh
+++ scripts/linux/container_install.sh
@@ -7,7 +7,7 @@ if [ "$#" -gt 0 ]; then
   BRANCH=$1
 fi
 
-yum -y install git
+apt-get -y install git
 git clone --depth=50 --branch=$BRANCH https://github.com/AO-StreetArt/CrazyIvan.git
 cd CrazyIvan/scripts/linux/deb && ./build_deps.sh
 export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
