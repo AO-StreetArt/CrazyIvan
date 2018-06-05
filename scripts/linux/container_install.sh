0fdcdb91e6f38b565e33e07f837ec469e8761b60
Alex Barry <aostreetart9@gmail.com>
2018-06-05 19:20:49 -0400

    more fixes for container_install

--- scripts/linux/container_install.sh
+++ scripts/linux/container_install.sh
@@ -13,12 +13,12 @@ apt-get update
 apt-get -y install software-properties-common
 add-apt-repository -y ubuntu-toolchain-r-test
 apt-get -y update
-apt-get -y install git gcc-6 g++6
+apt-get -y install git gcc-6 g++6 make
 ln -s /usr/bin/gcc-6 /usr/bin/gcc
 ln -s /usr/bin/g++-6 /usr/bin/g++
 git clone --depth=50 --branch=$BRANCH https://github.com/AO-StreetArt/CrazyIvan.git
 mkdir ivan_deps
 cp CrazyIvan/scripts/linux/deb/build_deps.sh ../ivan_deps
-cd ivan_deps && sudo ./build_deps.sh
+cd ivan_deps && ./build_deps.sh
 export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
 cd ../CrazyIvan && make && make test
