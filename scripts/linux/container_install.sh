2fe7a4f5dcc8606f17a31ed17076d2d06c374af7
Alex Barry <aostreetart9@gmail.com>
2018-06-05 02:47:53 -0400

    install ubuntu convenience script in container install

--- scripts/linux/container_install.sh
+++ scripts/linux/container_install.sh
@@ -8,11 +8,12 @@ if [ "$#" -gt 0 ]; then
 fi
 
 apt-get update
-add-apt-repository ubuntu-toolchain-r-test
-apt-get update
+apt-get -y install software-properties-common
+add-apt-repository -y ubuntu-toolchain-r-test
+apt-get -y update
 apt-get -y install git gcc-6 g++6
-sudo unlink /usr/bin/gcc && sudo ln -s /usr/bin/gcc-6 /usr/bin/gcc
-sudo unlink /usr/bin/g++ && sudo ln -s /usr/bin/g++-6 /usr/bin/g++
+unlink /usr/bin/gcc && sudo ln -s /usr/bin/gcc-6 /usr/bin/gcc
+unlink /usr/bin/g++ && sudo ln -s /usr/bin/g++-6 /usr/bin/g++
 git clone --depth=50 --branch=$BRANCH https://github.com/AO-StreetArt/CrazyIvan.git
 cd CrazyIvan/scripts/linux/deb && ./build_deps.sh
 export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
