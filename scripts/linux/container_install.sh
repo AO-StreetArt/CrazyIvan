f09c7a2d31fc5409319866f77f9a69f6ed8ee59c
Alex Barry <aostreetart9@gmail.com>
2018-06-05 19:19:12 -0400

    fixes for container install

--- scripts/linux/container_install.sh
+++ scripts/linux/container_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #This script will install Crazy Ivan by cloning it from github and building from source
 
+set -e
+
 BRANCH="v2"
 
 if [ "$#" -gt 0 ]; then
@@ -12,11 +14,11 @@ apt-get -y install software-properties-common
 add-apt-repository -y ubuntu-toolchain-r-test
 apt-get -y update
 apt-get -y install git gcc-6 g++6
-unlink /usr/bin/gcc && ln -s /usr/bin/gcc-6 /usr/bin/gcc
-unlink /usr/bin/g++ && ln -s /usr/bin/g++-6 /usr/bin/g++
+ln -s /usr/bin/gcc-6 /usr/bin/gcc
+ln -s /usr/bin/g++-6 /usr/bin/g++
 git clone --depth=50 --branch=$BRANCH https://github.com/AO-StreetArt/CrazyIvan.git
-mkdir ../ivan_deps
-cp scripts/linux/deb/build_deps.sh ../ivan_deps
-cd ../ivan_deps && sudo ./build_deps.sh
+mkdir ivan_deps
+cp CrazyIvan/scripts/linux/deb/build_deps.sh ../ivan_deps
+cd ivan_deps && sudo ./build_deps.sh
 export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
 cd ../CrazyIvan && make && make test
