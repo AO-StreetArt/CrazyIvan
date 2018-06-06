9fc06e22aa4d09d9769129794270432578881d2f
Alex Barry <aostreetart9@gmail.com>
2018-06-05 20:42:39 -0400

    minor container fixes

--- scripts/linux/container_install.sh
+++ scripts/linux/container_install.sh
@@ -18,7 +18,7 @@ ln -s /usr/bin/gcc-6 /usr/bin/gcc
 ln -s /usr/bin/g++-6 /usr/bin/g++
 git clone --depth=50 --branch=$BRANCH https://github.com/AO-StreetArt/CrazyIvan.git
 mkdir ivan_deps
-cp CrazyIvan/scripts/linux/deb/build_deps.sh ../ivan_deps
+cp CrazyIvan/scripts/linux/deb/build_deps.sh ivan_deps
 cd ivan_deps && ./build_deps.sh
 export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
 cd ../CrazyIvan && make && make test
