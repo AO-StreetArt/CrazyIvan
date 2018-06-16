b0a7d3686bb647e08c7df04771336874ba8cada8
Alex Barry <aostreetart9@gmail.com>
2018-06-16 02:14:33 -0400

    travis ci fix

--- tests/netcat/udp_test.sh
+++ tests/netcat/udp_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Setup the UDP message
-python register_device.py $(ip addr show enp2s0 | grep -Po 'inet \K[\d.]+')
+python register_device.py $(ip addr show eth0 | grep -Po 'inet \K[\d.]+')
 # Sleep long enough for the cache loader to load the scene
 sleep 10
 # Send the UDP Message with NetCat
-cat udp_message_upd.txt | nc -u $(ip addr show enp2s0 | grep -Po 'inet \K[\d.]+') 8764
+cat udp_message_upd.txt | nc -u $(ip addr show eth0 | grep -Po 'inet \K[\d.]+') 8764
 printf "Completing UDP Test -- Bash\n"
