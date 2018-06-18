7b075ee47c5d4fa04ca2b3cdf25e26919f8aafbf
Alex Barry <aostreetart9@gmail.com>
2018-06-17 22:01:36 -0400

    use python sleep to pause ci execution

---
+++ tests/netcat/sleep.py
@@ -0,0 +1,5 @@
+import time
+import sys
+
+if __name__ == "__main__":
+    time.sleep(float(sys.argv[1]))
--- tests/netcat/udp_test.sh
+++ tests/netcat/udp_test.sh
@@ -3,11 +3,11 @@
 # Setup the UDP message
 python register_device.py $(ip addr show eth0 | grep -Po 'inet \K[\d.]+')
 # Sleep long enough for the cache loader to load the scene
-sleep 10
+python sleep.py 10
 # Send the UDP Message with NetCat
 cat udp_message_upd.txt | nc -u $(ip addr show eth0 | grep -Po 'inet \K[\d.]+') 8764
 printf "Completing UDP Test -- Bash\n"
 # Sleep long enough to let messages send
-sleep 5
+python sleep.py 5
 # Remove the scene from the cache
 python remove_cached_scene.py $(cat scene_key.txt)
