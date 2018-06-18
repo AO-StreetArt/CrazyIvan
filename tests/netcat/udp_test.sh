d6c45be98cfa538c8033a8b66a47167f8d520263
Alex Barry <aostreetart9@gmail.com>
2018-06-18 00:01:14 -0400

    remove parts of udp tests

--- tests/netcat/udp_test.sh
+++ tests/netcat/udp_test.sh
@@ -7,9 +7,8 @@ printf "Sleeping"
 python sleep.py 10
 # Send the UDP Message with NetCat
 printf "Sending UDP message"
-cat udp_message_upd.txt | nc -u $(ip addr show eth0 | grep -Po 'inet \K[\d.]+') 8764
-printf "Completing UDP Test -- Bash\n"
-# Sleep long enough to let messages send
-python sleep.py 5
-# Remove the scene from the cache
-python remove_cached_scene.py $(cat scene_key.txt)
+NETWORK_INTERFACE_NAME=$(route | grep '^default' | grep -o '[^ ]*$')
+NETWORK_INTERFACE_ADDRESS=$(ip addr show $NETWORK_INTERFACE_NAME | grep -Po 'inet \K[\d.]+')
+printf $NETWORK_INTERFACE_NAME
+printf $NETWORK_INTERFACE_ADDRESS
+cat udp_message_upd.txt | nc -u $NETWORK_INTERFACE_ADDRESS 8764
