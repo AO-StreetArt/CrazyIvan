c80629624db4009eed8b53b229a84b9065681ec1
Alex Barry <aostreetart9@gmail.com>
2018-10-03 15:43:57 -0400

    fix docker push script in CI

--- scripts/linux/push_docker_image.sh
+++ scripts/linux/push_docker_image.sh
@@ -3,7 +3,6 @@
 DOCKER_USER=$1
 DOCKER_PASS=$2
 BRANCH_NAME=$3
-DIR=$4
 
 TAG_NAME="v2"
 
@@ -13,5 +12,5 @@ if [ $BRANCH_NAME != "v2" ]; then
   TAG_NAME=$BRANCH_NAME
 fi
 
-cd $DIR && docker login -u $DOCKER_USER -p $DOCKER_PASS
-cd $DIR && docker push aostreetart/crazyivan:$TAG_NAME
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+docker push aostreetart/crazyivan:$TAG_NAME
